11c6e55f8ec2b8474c9d3e28f91cf8fe051387b1
JFang1 <jmf133@pitt.edu>
2017-01-30 10:44:57 -0500

    Cleans up the code a bit

--- LazyTriangle.java
+++ LazyTriangle.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Ryo112358
+ * @author Ryo112358, JFang1
  *
  */
 
@@ -8,23 +8,22 @@ public class LazyTriangle {
 
 	public static void main(String[] args) {
 
+		// ensuring that the correct arguments have been entered
 		int number = 0;
-
 		try {
 			number = Integer.parseInt(args[1]);
-
 		} catch (NumberFormatException nfe) {
 			System.exit(2);
 		}
 
-		//--------------------------------------
-
+		// printing out the results
+		int result = -1;
 		if (args[0].equals("lazy")) {
-			int result = lazyCaterer(number);
+			result = lazyCaterer(number);
 			System.out.println("The lazy caterer brings your bill...");
 			System.out.println("Lazy(" + number + ") = " + result + "!!!");
 		} else if (args[0].equals("triangle")){
-			int result = triangle(number);
+			result = triangle(number);
 			System.out.println("The triangle ran out of edges... ");
 			System.out.println("Tri(" + number + ") = " + result);
 		} else {
