3413b9ea856c40bf29e132883bfe97df1ce753ae
Ryo112358 <ryo112358@live.com>
2017-01-30 10:36:53 -0500

    A few fixes. Stable.

--- LazyTriangle.java
+++ LazyTriangle.java
@@ -21,10 +21,12 @@ public class LazyTriangle {
 
 		if (args[0].equals("lazy")) {
 			int result = lazyCaterer(number);
-			System.out.println("The lazy caterer brings your bill... $" + result + "!!!");
+			System.out.println("The lazy caterer brings your bill...");
+			System.out.println("Lazy(" + number + ") = " + result + "!!!");
 		} else if (args[0].equals("triangle")){
 			int result = triangle(number);
-			System.out.println("The triangle ran out of edges... " + result);
+			System.out.println("The triangle ran out of edges... ");
+			System.out.println("Tri(" + number + ") = " + result);
 		} else {
 			System.exit(1);
 		}
@@ -38,6 +40,7 @@ public class LazyTriangle {
 
 		while(number != 0) {
 			total += number;
+			number--;
 		}
 
 		return total;
